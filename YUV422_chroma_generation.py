8710fbb6630465319ce07b2e78ed08357fad1422
bruhjitp <brajitpaul10.bp@gmail.com>
2024-07-03 22:33:45 +0530

    Update and rename YUV444_chroma_generation.py to YUV422_chroma_generation.py

--- YUV422_chroma_generation.py
+++ YUV422_chroma_generation.py
@@ -1,7 +1,7 @@
 #random yuv444 chroma component generated for the conversions
 import random
 
-file=open("yuv444_chroma.ppm","w")
+file=open("yuv422_chroma.ppm","w")
 for i in range(0,256): #8 bit data
   num=random.randrange(0,640*480/2) #resolution 480p
   file.write(str(num)+'\n')
